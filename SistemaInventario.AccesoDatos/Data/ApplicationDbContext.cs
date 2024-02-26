5d42b364881bc01372dd7a8063e7b2436194a2c6
juanjociber <i202015638@cibertec.edu.pe>
2024-02-25 20:33:16 -0500

    Fluent API - Configuracion a Proyecto

---
+++ SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SistemaInventario.Modelos;
+using System.Reflection;
+
+namespace SistemaInventario.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        //Se agregan los Modelos
+        public DbSet<Bodega> Bodegas { get; set; }
+
+        //Fluent API
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
