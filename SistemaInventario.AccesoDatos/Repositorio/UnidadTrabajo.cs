1e91e7da18dd09068bebd25fe6ea99394e848576
juanjociber <i202015638@cibertec.edu.pe>
2024-02-26 21:34:12 -0500

    Patron Repositorio

---
+++ SistemaInventario.AccesoDatos/Repositorio/UnidadTrabajo.cs
@@ -0,0 +1,34 @@
+﻿using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio
+{
+    public class UnidadTrabajo : IUnidadTrabajo
+    {
+        private readonly ApplicationDbContext _db;
+        public IBodegaRepositorio Bodega { get; private set; }
+
+        public UnidadTrabajo(ApplicationDbContext db)
+        {
+            _db = db;
+            Bodega = new BodegaRepositorio(_db);
+        }   
+
+        public IBodegaRepositorio Bodega => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Guardar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
