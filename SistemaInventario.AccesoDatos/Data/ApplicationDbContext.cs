2039743ae8e68c9c920f22549877d4e8590d252b
juanjociber <i202015638@cibertec.edu.pe>
2024-02-29 22:44:38 -0500

    CRUD Categoria y Marca

--- SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
+++ SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
@@ -7,13 +7,15 @@ namespace SistemaInventario.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
         {
         }
 
-        //Se agregan los Modelos
+        //Espacio para todos los Modelos
         public DbSet<Bodega> Bodegas { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Marca> Marcas { get; set; } 
+
 
         //Fluent API
         protected override void OnModelCreating(ModelBuilder builder)
---
+++ SistemaInventario.Modelos/Marca.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Marca
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Nombre es obligatorio")]
+        [MaxLength(60,ErrorMessage ="Nombre debe tener máximo 60 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage ="Descripcion es obligatorio")]
+        [MaxLength(100,ErrorMessage ="Descripción debe tener máximo 100 caracteres")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage ="Estado es obligatorio")]
+        public bool Estado { get; set; }
+    }
+}
